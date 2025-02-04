c737124f6c77a1f14590f3a5d9c358d7f2c6ccd3
Sonal Sood <sonalsood97@gmail.com>
2025-02-04 15:54:22 -0500

    MM:26 setup /api/inventories/:id API endpoint

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -23,4 +23,37 @@ const index = async (_req, res) => {
     res.status(400).send("Error retrieving inventory items.");
   }
 };
-export { index };
+
+// GET single inventory item
+const findOne = async (req, res) => {
+  try {
+    const inventoryFound = await knex("inventories")
+      .join("warehouses", "warehouses.id", "inventories.warehouse_id")
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      )
+      .where("inventories.id", req.params.id);
+
+    if (inventoryFound.length === 0) {
+      return res.status(404).json({
+        message: `Inventory with ID ${req.params.id} not found`,
+      });
+    }
+
+    const inventoryData = inventoryFound[0];
+    res.status(200).json(inventoryData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: `Unable to retrieve inventory data for inventory with ID ${req.params.id}`,
+    });
+  }
+};
+
+export { index, findOne };
