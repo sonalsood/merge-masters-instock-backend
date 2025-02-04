2e12820bce2466e4da1ab9a3c338def29609a6d4
FarinazCodes <hello.farinaz@gmail.com>
2025-02-04 14:37:45 -0500

    Below files updated for API to GET List of all Inventory Items Warehouse-controller.js Warehouse-routes.js Index.js Knexfqile.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import warehousesRoute from "./routes/warehouse-routes.js";
+import inventoryRoute from "./routes/inventory-routes.js";
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use("/api/warehouses", warehousesRoute);
+app.use("/api/inventories", inventoryRoute);
 app.use(cors());
 
 app.use(express.json());
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,2 +1,9 @@
 import express from "express";
 const router = express.Router();
+import * as inventoryController from "../controllers/inventory-controller.js";
+
+// GET /api/warehouses
+router.get("/", inventoryController.index);
+
+export default router;
+$;
