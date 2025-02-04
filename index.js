1a3d2dd86f747b51152c7bad068abca61f64270c
FarinazCodes <hello.farinaz@gmail.com>
2025-02-04 13:47:17 -0500

    /api is removed based on the feedback

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import warehousesRoute from "./routes/warehouse-routes.js";
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-app.use("/api/warehouses", warehousesRoute);
+app.use("/warehouses", warehousesRoute);
 app.use(cors());
 
 app.use(express.json());
