ee01fe7bff7a1c78fc001468ff4891b6e8e20e19
FarinazCodes <hello.farinaz@gmail.com>
2025-02-04 13:17:31 -0500

    Below files updated to GET List of All Warehouses Warehouse-controller.js Warehouse-routes.js Index.js Knexfqile.js

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,1 +1,27 @@
 import initKnex from "knex";
+import configuration from "../knexfile.js";
+
+const knex = initKnex(configuration);
+
+// GET all warehouse items
+const index = async (_req, res) => {
+  try {
+    const items = await knex("warehouses").select(
+      "id",
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_name",
+      "contact_position",
+      "contact_phone",
+      "contact_email"
+    );
+    res.status(200).json(items);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error getting warehouse items.");
+  }
+};
+
+export { index };
