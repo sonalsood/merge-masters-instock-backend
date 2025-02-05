fd12cf7f4de383b1c0832705202a0b1c7ad3653e
ChaofanMa <chaofanmah@gmail.com>
2025-02-05 01:05:37 -0500

    finish post warehouse function

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import warehousesRoute from "./routes/warehouse-routes.js";
 import inventoryRoute from "./routes/inventory-routes.js";
 const app = express();
 const PORT = process.env.PORT || 5050;
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/warehouses", warehousesRoute);
 app.use("/api/inventories", inventoryRoute);
-app.use(cors());
 
-app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("merge-masters-instock-backend API HOMEPAGE");
