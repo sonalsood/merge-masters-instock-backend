10619b4f7226e57216571a5e9890cc246b256808
ChaofanMa <chaofanmah@gmail.com>
2025-02-07 12:10:53 -0500

    optimise router setting

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -2,14 +2,11 @@ import express from "express";
 const router = express.Router();
 import * as inventoryController from "../controllers/inventory-controller.js";
 
-// GET /api/inventories
-router.get("/", inventoryController.index);
-
-// GET /api/inventories/:id
-router.get("/:id", inventoryController.findOne);
-
-// POST /api/inventories
-router.post("/", inventoryController.addInventory);
+// GET /api/inventories // POST /api/inventories
+router
+  .route("/")
+  .get(inventoryController.index)
+  .post(inventoryController.addInventory);
 
 // GET /inventories/:id  PUT /inventories/:id DELETE /inventories/:id
 router
@@ -17,6 +14,5 @@ router
   .get(inventoryController.findOne)
   .put(inventoryController.editInventory)
   .delete(inventoryController.deleteInventory);
-  
 
 export default router;
