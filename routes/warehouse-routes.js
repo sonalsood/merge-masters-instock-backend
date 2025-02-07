48cad5ecb99cb3e84e7a6170a1174da9427b4c3f
ChaofanMa <chaofanmah@gmail.com>
2025-02-07 11:26:41 -0500

    optimised routes setting for same end point with different request

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -8,13 +8,12 @@ router.get("/", warehouseController.index);
 //POST /api/warehouses
 router.post("/", warehouseController.addWarehouse);
 
-// GET AND DELETE /warehouses/:id
-router.route("/:id").get(warehouseController.findOne).delete(warehouseController.deleteWarehouse);
-// GET /warehouses/:id  PUT /warehouses/:id
+// GET AND DELETE /warehouses/:id  PUT /warehouses/:id
 router
   .route("/:id")
   .get(warehouseController.findOne)
-  .put(warehouseController.editWarehouse);
+  .put(warehouseController.editWarehouse)
+  .delete(warehouseController.deleteWarehouse);
 
 //GET warehouses/:id/inventories
 router.route("/:id/inventories").get(warehouseController.inventory);
